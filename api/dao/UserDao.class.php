53586a4ead97145c6c984a7ebf05041cf5b60f42
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-03-14 18:28:49 +0100

    incremental updates -> update_user

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -29,15 +29,28 @@ class UserDao extends BaseDao
 
     public function update_user($id, $user) 
     {
+
+        //generating automated query
+        $query = "UPDATE users SET ";
+        
+        foreach ($user as $column => $value) {
+            $query.= $column." = :".$column.", ";
+        }
+        
+        $query = mb_substr($query, 0, -2);
+        $query .= " WHERE id = :id";
+
+        //executing the query
+        $stmt= $this->connection->prepare($query);
+        $user["id"] = $id;
+        $stmt->execute($user);
+
+        return $user;
+
+        /*
         $sql = "UPDATE users 
                 SET username = :username, email = :email, password = :password, account_id = :account_id
                 WHERE id = :id";
-
-        $stmt= $this->connection->prepare($sql);
-        $user["id"] = $id;
-        $stmt->execute($user);
-        
-
-        return $user;
+        */
     }
 }
