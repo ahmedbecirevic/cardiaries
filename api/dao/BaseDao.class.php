d4eb547deda189b79fa1a655c37f5530152df868
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-03-17 17:26:29 +0100

    generic insert in BaseDao used in UserDao

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -20,9 +20,24 @@ class BaseDao
 
     }
 
-    public function insert()
+    public function insert($table, $entity)
     {
+        $query = "INSERT INTO $table (";
 
+        foreach ($entity as $column => $value) {
+            $query .= "$column, ";
+        }
+        $query = substr($query, 0, -2) . ") VALUES (";
+
+        foreach ($entity as $column => $value) {
+            $query .= ":$column, ";
+        }
+        $query = substr($query, 0, -2) . ")";
+
+        $stmt= $this->connection->prepare($query);
+        $stmt->execute($entity);         //prevent sql injection
+        $entity["id"] = $this->connection->lastInsertId();
+        return $entity;
     }
 
     public function update($table, $id, $entity, $id_column = "id")
@@ -41,8 +56,6 @@ class BaseDao
         $stmt= $this->connection->prepare($query);
         $entity['id'] = $id;
         $stmt->execute($entity);
-
-        return $entity;
     }
 
 
@@ -57,7 +70,6 @@ class BaseDao
     public function query_unique($query, $params) 
     {
         $result = $this->query($query, $params);
-
         return reset($result); // reset - returns first element of array, checks if null etc. 
 
     }
