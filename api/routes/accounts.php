5940f77917b6d6c02903ad5f805d55d19936ba01
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-03-23 17:55:43 +0100

    dummy account validation

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -10,21 +10,18 @@ Flight::route('GET /accounts', function(){
 });
 
 Flight::route('GET /accounts/@id', function($id) {
-    Flight::json(Flight::accountDao()->get_by_id($id));
+    Flight::json(Flight::accountService()->get_by_id($id));
 });
 
 Flight::route('POST /accounts', function(){
     $data = Flight::request()->data->getData();
-    Flight::json(Flight::accountDao()->add($data));
+    Flight::json(Flight::accountService()->add($data));
 
 });
 
 //Route to update account table
 Flight::route('PUT /accounts/@id', function($id){
-    $request = Flight::request();
-    $data = $request->data->getData();
+    $data = Flight::request()->data->getData();
 
-    Flight::accountDao()->update($id, $data);
-
-    Flight::json(Flight::accountDao()->get_by_id($id));
+    Flight::json(Flight::accountService()->update($id, $data));
 });
--- api/services/AccountService.class.php
+++ api/services/AccountService.class.php
@@ -1,19 +1,32 @@
 <?php
 
-require_once dirname(__FILE__).'/../dao/AccountDao.class.php';
+require_once dirname(__FILE__) . '/BaseService.class.php';
+
+require_once dirname(__FILE__) . '/../dao/AccountDao.class.php';
 
 
-class AccountService {
-    private $dao;
-    public function __construct() {
+class AccountService extends BaseService
+{
+
+    public function __construct()
+    {
         $this->dao = new AccountDao();
     }
 
-    public function get_accounts($search, $offset, $limit) {
-        if($search) {
-            return $this->dao->get_accounts($search, $offset, $limit);     
+    public function get_accounts($search, $offset, $limit)
+    {
+        if ($search) {
+            return $this->dao->get_accounts($search, $offset, $limit);
         } else {
-            return $this->dao->get_all($offset, $limit);   
+            return $this->dao->get_all($offset, $limit);
         }
+    }
+
+    public function add($account)
+    {
+        // perform validation of account data
+        if (!isset($account['name'])) throw new Exception("Name is missing!!!");
+
+        return parent::add($account);
     }
 }
