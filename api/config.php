448794ff88e12795872517c7058ed5ff507ca778
Ahmed Becirevic <64833009+ahmedbecirevic@users.noreply.github.com>
2021-04-28 14:00:31 +0200

    config file improve

--- api/config.php
+++ api/config.php
@@ -1,10 +1,49 @@
 <?php
-
 class Config
 {
     const DATE_FORMAT = "Y-m-d H:i:s";
-    const DB_HOST = "localhost";
-    const DB_USERNAME = "cardiaries";
-    const DB_PASSWORD = "cardiaries";
-    const DB_SCHEME = "cardiaries";
+    const JWT_SECRET = "dk*M6spn7qk6b$<5";
+    const JWT_TOKEN_TIME = 604800;
+
+    public static function DB_HOST()
+    {
+        return Config::get_env("DB_HOST", "localhost");
+    }
+    public static function DB_USERNAME()
+    {
+        return Config::get_env("DB_USERNAME", "cardiaries");
+    }
+    public static function DB_PASSWORD()
+    {
+        return Config::get_env("DB_PASSWORD", "cardiaries");
+    }
+    public static function DB_SCHEME()
+    {
+        return Config::get_env("DB_SCHEME", "cardiaries");
+    }
+    public static function DB_PORT()
+    {
+        return Config::get_env("DB_PORT", "3306");
+    }
+    public static function SMTP_HOST()
+    {
+        return Config::get_env("SMTP_HOST", "smtp.mailgun.org");
+    }
+    public static function SMTP_PORT()
+    {
+        return Config::get_env("SMTP_PORT", "587");
+    }
+    public static function SMTP_USER()
+    {
+        return Config::get_env("SMTP_USER", NULL);
+    }
+    public static function SMTP_PASSWORD()
+    {
+        return Config::get_env("SMTP_PASSWORD", NULL);
+    }
+
+    public static function get_env($name, $default)
+    {
+        return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+    }
 }
