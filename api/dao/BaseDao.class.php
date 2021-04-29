e5303f7b288a7352109fc58ab311d4f99604f9aa
Ahmed Becirevic <64833009+ahmedbecirevic@users.noreply.github.com>
2021-04-29 12:56:17 +0200

    error fix

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -17,7 +17,7 @@ class BaseDao
     {
         $this->table = $table;
         try {
-            $this->connection = new PDO("mysql:host=" . Config::DB_HOST . ";dbname=" . Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
+            $this->connection = new PDO("mysql:host=" . Config::DB_HOST() . ";dbname=" . Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             date_default_timezone_set("Europe/Sarajevo");
             // $this->connection->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
