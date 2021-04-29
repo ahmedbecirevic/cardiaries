34a0c0f6f70c7e2de593b33f79da4d2ef322efd8
Ahmed Becirevic <64833009+ahmedbecirevic@users.noreply.github.com>
2021-04-29 19:49:06 +0200

    test DO

--- api/routes/posts.php
+++ api/routes/posts.php
@@ -1,11 +1,33 @@
 <?php
 
 /**
- * @OA\Get(path="/posts/users/{id}", tags={"post"}, 
+ * @OA\Get(path="/post/users/{id}", tags={"post"}, 
  *    @OA\Parameter(type="integer", in="path", name="id", default=1, description="ID of user"),
  *    @OA\Response(response="200", description="Fetch individual user")
  * )
  */
-Flight::route('GET /posts/users/@id', function ($id) {
+Flight::route('GET /user/posts/@id', function ($id) {
     Flight::json(Flight::userService()->get_by_id($id));
+});
+
+/**
+ * 
+ * @OA\Post(path="/user/post", tags={"post"}, security={{"ApiKeyAuth": {}}},
+ *      @OA\RequestBody(
+ *          description="Post fields",
+ *          required=true,
+ *          @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    				@OA\Property(property="body", required="true", type="text", example="Writing about my car bla bla", description="Body of the post"),
+ * 
+ *              )
+ *          )
+ *      ),
+ *      @OA\Response(response="200", description="Account that has been added to the database with ID assigned")
+ * )
+ * 
+ */
+Flight::route('POST /accounts', function () {
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::postService()->add($data));
 });
--- api/services/PostService.class.php
+++ api/services/PostService.class.php
@@ -1,15 +1,24 @@
 <?php
 
 require_once dirname(__FILE__) . '/BaseService.class.php';
-
 require_once dirname(__FILE__) . '/../dao/CarDao.class.php';
+require_once dirname(__FILE__) . '/../dao/ImageDao.class.php';
 
 
 class PostService extends BaseService
 {
+    private $imageDao;
 
     public function __construct()
     {
         $this->dao = new PostsDao();
+        $this->imageDao = new ImageDao();
+    }
+
+    public function add($post)
+    {
+        if (!isset($post['body'])) throw new Exception("Post body is missing!!!");
+        $post['created_at'] = date(Config::DATE_FORMAT);
+        return parent::add($post);
     }
 }
