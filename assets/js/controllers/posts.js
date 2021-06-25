6ad534a4493df27cbdd7f50f65ff31017a5f1f53
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-06-25 16:18:09 +0200

    modal clear, many bug fixes

--- assets/js/controllers/posts.js
+++ assets/js/controllers/posts.js
@@ -1,5 +1,8 @@
 class Posts {
    static init () {
+      Posts.getPosts();
+      Posts.modalEnable();
+
       $("#add-post-form").validate({
          submitHandler: function(form, event) {
             event.preventDefault();
@@ -12,27 +15,25 @@ class Posts {
             }
          }
       });
-      Posts.getPosts();
-      Posts.modalEnable();
    }
     
    static getPosts () {
       RestClient.get('api/user/posts', function(data) {
          console.log(data);
+         $("#posts-group-by-car").empty();
          for (let i = 0; i < data.length; i++) {
             const carId = data[i].car_id;
             let html = "";
             let postHtml = "";
             if (document.getElementById("car" + carId)) { //retrns true if there is elements with such ID which means that we already have inserted other posts for this car
                let insertPostSelector = "#post-insert" + carId;
-               console.log(insertPostSelector);
-               postHtml += `<p class="card-text"> ${data[i].body} </p><p class="card-text"><small class="text-muted">Post created at:  ${data[i].created_at} </small></p><img id="image" class="card-img-bottom" src="./assets/img/e92m3.jpg" alt="Card image cap">`
+               postHtml += `<p class="card-text"> ${data[i].body} </p><p class="card-text"><small class="text-muted">Post created at:  ${data[i].created_at} </small></p><img id="image" class="card-img-bottom" src="${data[i].image_url}" alt="Card image cap">`
                $(insertPostSelector).append(postHtml);
             } else {
                // this case handles inserting the post which is the first for the car that it belongs to
                let elementCarId = "car" + carId;
                let postId = "post-insert" + carId;
-               html += `<div class="card mb-4"><div class="card-header"><i class="fas fa-car mr-1"></i><p id="${elementCarId}">${carId}</p> posts!</div><div class="card"><div class="card-body" id="${postId}"><button id="add-post-modal-launch" onclick="Posts.preEditCarIdToPost(${data[i].car_id})" class="btn btn-success my-2" aria-hidden="true" data-bs-toggle="modal" data-bs-target="#addPostModal">+ Add Post</button><p class="card-text"> ${data[i].body} </p><p class="card-text"><small class="text-muted">Post created at:  ${data[i].created_at} </small></p><img id="image" class="card-img-bottom" src="./assets/img/e92m3.jpg" alt="Card image cap"></div></div></div>`;
+               html += `<div class="card mb-4"><div class="card-header"><i class="fas fa-car mr-1"></i><p id="${elementCarId}">${carId}</p> posts!</div><div class="card"><div class="card-body" id="${postId}"><button id="add-post-modal-launch" onclick="Posts.preEditCarIdToPost(${data[i].car_id})" class="btn btn-success my-2" aria-hidden="true" data-bs-toggle="modal" data-bs-target="#addPostModal">+ Add Post</button><p class="card-text"> ${data[i].body} </p><p class="card-text"><small class="text-muted">Post created at:  ${data[i].created_at} </small></p><img id="image" class="card-img-bottom" src="${data[i].image_url}" alt="Card image cap"></div></div></div>`;
                $("#posts-group-by-car").append(html);
             }
          }
@@ -47,12 +48,11 @@ class Posts {
       const carId = post['car_id'];
       delete post['car_id'];
       delete post['id'];
-      console.log("in the addPost: " + post);
       RestClient.post("api/user/car/posts/" + carId, post, function (data) {
          toastr.success("New post has been added!");
-         Posts.getPosts();
-         $("#add-email-template").trigger("reset");
+         $("#add-post-form").trigger("reset");
          $('#addPostModal').modal("hide");
+         Posts.getPosts();
       })
    }
 
@@ -64,6 +64,7 @@ class Posts {
    }
 
    static preEditCarIdToPost (carId) {
+      $("#add-post-button").prop('disabled', true);  
       $("#add-post-form *[name='car_id']").val(carId);
       $("#addPostModal").modal("show");
    }
@@ -123,9 +124,18 @@ class Posts {
          $('#addPostModal').modal({show : true});
       });
 
-      $("#reset-form-modal").click(function () {
-         $("#add-post-form")[0].reset();
-      });
+      $("#reset-form-modal").click(Posts.clearModalForm);
+
+      $("#x-reset-form-modal").click(Posts.clearModalForm);
+   }
+
+   static clearModalForm () {
+      $("#add-post-form").trigger("reset");
+         document.getElementById('hidden_id').value = "";
+         document.getElementById('hidden_car_id').value = "";
+         document.getElementById('post-body').value = "";
+         $('#upload-img').attr('src', '');
+         document.getElementById('image_url_id').value = "";
    }
 
 }
